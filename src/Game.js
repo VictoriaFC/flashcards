d84df0a48098ac4b772ace36e299731b700aaa0b
Victoria Fox-Collis <victoriafoxc@gmail.com>
2022-05-12 17:58:30 -0600

    Add currentRound to constructor and wrote start method

--- src/Game.js
+++ src/Game.js
@@ -1,9 +1,14 @@
 const data = require('./data');
 const prototypeQuestions = data.prototypeData;
 const util = require('./util');
+const Card = require('../src/Card');
+const Deck = require('../src/Deck');
+const Round = require('../src/Round');
 
 class Game {
-  constructor() {}
+  constructor() {
+    this.currentRound = null
+  }
 
   printMessage(deck, round) {
     console.log(`Welcome to FlashCards! You are playing with ${deck.countCards()} cards.
@@ -14,16 +19,15 @@ class Game {
       util.main(round);
   }
 
-  currentRound() {
-    // keeps track of the current round
-  }
-
   start() {
-    // Creates Cards
-    // Puts Cards in a Deck
-    // Creates a new Round using the Deck
-    // invokes printMessage to display the message in the CLI
-    // invokes printQuestion to kick off our helper functions that allow interaction via the CLI
+    const cards = prototypeQuestions.map((cardObj) => {
+      return new Card(cardObj.id, cardObj.question, cardObj.answers, cardObj.correctAnswer);
+    })
+    const deck = new Deck(cards);
+    this.currentRound = new Round(deck);
+
+    this.printMessage(deck, this.currentRound)
+    this.printQuestion(this.currentRound)
   }
 }
 
--- test/Game-test.js
+++ test/Game-test.js
@@ -6,36 +6,21 @@ const Deck = require('../src/Deck');
 const Round = require('../src/Round');
 const Game = require('../src/Game');
 
-
-
 describe('Game', function() {
 
   it('should be a function', function() {
-    const game = new Game({});
+    const game = new Game();
     expect(Game).to.be.a('function');
   });
 
   it('should be an instance of Game', function() {
-    const game = new Game({});
+    const game = new Game();
     expect(game).to.be.an.instanceof(Game);
   });
 
-  it.skip('should be able to keep track of the round', function() {
-    const card1 = new Card(1, 'What is Robbie\'s favorite animal', ['sea otter', 'pug', 'capybara'], 'sea otter');
-    const card2 = new Card(14, 'What organ is Khalid missing?', ['spleen', 'appendix', 'gallbladder'], 'gallbladder');
-    const card3 = new Card(12, 'What is Travis\'s favorite stress reliever?', ['listening to music', 'watching Netflix', 'playing with bubble wrap'], 'playing with bubble wrap');
-    const deck = new Deck([card1, card2, card3]);
-    const round = new Round(deck);
-    const game = new Game({});
-    expect(game.currentRound()).to.equal();
-  });
+  it('should have a current round to be undefined', function() {
+    const game = new Game();
 
-  it.skip('should be able to start the game', function() {
-    const card1 = new Card(1, 'What is Robbie\'s favorite animal', ['sea otter', 'pug', 'capybara'], 'sea otter');
-    const card2 = new Card(14, 'What organ is Khalid missing?', ['spleen', 'appendix', 'gallbladder'], 'gallbladder');
-    const card3 = new Card(12, 'What is Travis\'s favorite stress reliever?', ['listening to music', 'watching Netflix', 'playing with bubble wrap'], 'playing with bubble wrap');
-    const deck = new Deck([card1, card2, card3]);
-    const round = new Round(deck);
-    expect(game.start()).to.equal(0);
+    expect(game.currentRound).to.equal(null);
   });
 });
